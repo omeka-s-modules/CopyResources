fcd6493653d39da6218f6d2709e36f551e842c79
Jim Safley <jimsafley@gmail.com>
2024-02-29 06:26:50 -0500

    Add permission checks for lower roles

--- Module.php
+++ Module.php
@@ -3,6 +3,7 @@ namespace CopyResources;
 
 use Laminas\EventManager\Event;
 use Laminas\EventManager\SharedEventManagerInterface;
+use Laminas\Mvc\MvcEvent;
 use Omeka\Module\AbstractModule;
 
 class Module extends AbstractModule
@@ -12,14 +13,42 @@ class Module extends AbstractModule
         return include sprintf('%s/config/module.config.php', __DIR__);
     }
 
+    public function onBootstrap(MvcEvent $event)
+    {
+        parent::onBootstrap($event);
+        $acl = $this->getServiceLocator()->get('Omeka\Acl');
+
+        // Allow all roles to access copy actions. Rely on native permissions to
+        // restrict resource creation should someone directly access copy forms.
+        $acl->allow(null, 'CopyResources\Controller\Admin\Index', ['copy-confirm', 'copy']);
+    }
+
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
+        $acl = $this->getServiceLocator()->get('Omeka\Acl');
+
         // Add copy icons to resource browse pages.
         $copyEvents = [
-            ['controller' => 'Omeka\Controller\Admin\Item', 'resource_name' => 'items'],
-            ['controller' => 'Omeka\Controller\Admin\ItemSet', 'resource_name' => 'item_sets'],
-            ['controller' => 'Omeka\Controller\SiteAdmin\Page', 'resource_name' => 'site_pages'],
-            ['controller' => 'Omeka\Controller\SiteAdmin\Index', 'resource_name' => 'sites'],
+            [
+                'controller' => 'Omeka\Controller\Admin\Item',
+                'resource_name' => 'items',
+                'is_allowed_callback' => fn ($resource) => $resource->userIsAllowed('create'),
+            ],
+            [
+                'controller' => 'Omeka\Controller\Admin\ItemSet',
+                'resource_name' => 'item_sets',
+                'is_allowed_callback' => fn ($resource) => $resource->userIsAllowed('create'),
+            ],
+            [
+                'controller' => 'Omeka\Controller\SiteAdmin\Page',
+                'resource_name' => 'site_pages',
+                'is_allowed_callback' => fn ($resource) => $resource->site()->userIsAllowed('add-page'),
+            ],
+            [
+                'controller' => 'Omeka\Controller\SiteAdmin\Index',
+                'resource_name' => 'sites',
+                'is_allowed_callback' => fn ($resource) => $acl->userIsAllowed('Omeka\Entity\Site', 'create'),
+            ],
         ];
         foreach ($copyEvents as $copyEvent) {
             $sharedEventManager->attach(
@@ -27,7 +56,7 @@ class Module extends AbstractModule
                 'view.browse.actions',
                 function (Event $event) use ($copyEvent) {
                     $resource = $event->getParam('resource');
-                    if (!$resource->userIsAllowed('create')) {
+                    if (!$copyEvent['is_allowed_callback']($resource)) {
                         return;
                     }
                     $view = $event->getTarget();
@@ -44,10 +73,30 @@ class Module extends AbstractModule
 
         // Add copy buttons to resource show pages.
         $copyEvents = [
-            ['controller' => 'Omeka\Controller\Admin\Item', 'event' => 'view.show.page_actions', 'resource_name' => 'items'],
-            ['controller' => 'Omeka\Controller\Admin\ItemSet', 'event' => 'view.show.page_actions', 'resource_name' => 'item_sets'],
-            ['controller' => 'Omeka\Controller\SiteAdmin\Page', 'event' => 'view.edit.page_actions', 'resource_name' => 'site_pages'],
-            ['controller' => 'Omeka\Controller\SiteAdmin\Index', 'event' => 'view.show.page_actions', 'resource_name' => 'sites'],
+            [
+                'controller' => 'Omeka\Controller\Admin\Item',
+                'event' => 'view.show.page_actions',
+                'resource_name' => 'items',
+                'is_allowed_callback' => fn ($resource) => $resource->userIsAllowed('create'),
+            ],
+            [
+                'controller' => 'Omeka\Controller\Admin\ItemSet',
+                'event' => 'view.show.page_actions',
+                'resource_name' => 'item_sets',
+                'is_allowed_callback' => fn ($resource) => $resource->userIsAllowed('create'),
+            ],
+            [
+                'controller' => 'Omeka\Controller\SiteAdmin\Page',
+                'event' => 'view.edit.page_actions',
+                'resource_name' => 'site_pages',
+                'is_allowed_callback' => fn ($resource) => $resource->site()->userIsAllowed('add-page'),
+            ],
+            [
+                'controller' => 'Omeka\Controller\SiteAdmin\Index',
+                'event' => 'view.show.page_actions',
+                'resource_name' => 'sites',
+                'is_allowed_callback' => fn ($resource) => $acl->userIsAllowed('Omeka\Entity\Site', 'create'),
+            ],
         ];
         foreach ($copyEvents as $copyEvent) {
             $sharedEventManager->attach(
@@ -56,7 +105,7 @@ class Module extends AbstractModule
                 function (Event $event) use ($copyEvent) {
                     $view = $event->getTarget();
                     $resource = $event->getParam('resource');
-                    if (!$resource->userIsAllowed('create')) {
+                    if (!$copyEvent['is_allowed_callback']($resource)) {
                         return;
                     }
                     echo $view->hyperlink($view->translate('Copy'), '#', [
